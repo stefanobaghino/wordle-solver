4cd27dee82a1375e7054c7f7675d3d492f2f4830
Stefano Baghino <stefano@baghino.me>
2022-01-05 22:47:17 +0100

    first implementation

---
+++ README.md
@@ -0,0 +1,52 @@
+### Wordle Solver
+
+Solver for [Wordle](https://www.powerlanguage.co.uk/wordle/).
+
+#### How to use it
+
+The easiest way is to run `sbt console`.
+
+When in the Scala shell, first `import me.baghino.Wordle`.
+
+Initialize a new game by loading a dictionary file, as in the following example:
+
+```
+val wordle = Wordle.loadAndCleanDictionary("/usr/share/dict/british-english")
+```
+
+The dictionary file is a list of words which in Linux systems is commonly found under `/usr/share/dict`. The
+dictionary file is automatically cleaned for you (i.e. only 5-letter words are kept).
+
+The constructor itself prints the first guess. Input the guess on the site and feed the information from the site back
+into the program with the `feedback` method, which asks for a few optional inputs: letters that have been ruled out
+("doesNotMatch"), letters that are in but at an unknown position ("fuzzyMatch") and letters which are already known to
+be in the right spot ("at0" through "at4").
+
+The following is a session I played in the past:
+
+```
+scala> import me.baghino.Wordle
+import me.baghino.Wordle
+
+scala> val wordle = Wordle.loadAndCleanDictionary("/usr/share/dict/british-english")
+My next guess is 'arose'.
+val wordle: me.baghino.Wordle = me.baghino.Wordle@5d7e4ab5
+
+scala> wordle.feedback(doesNotMatch = "aos", fuzzyMatch = "re")
+My next guess is 'until'.
+
+scala> wordle.feedback(doesNotMatch = "unl", fuzzyMatch = "ti")
+My next guess is 'pygmy'.
+
+scala> wordle.feedback(doesNotMatch = "pymy", at2 = 'g')
+My next guess is 'tiger'.
+```
+
+And voila: "tiger" is indeed the correct answer.
+
+#### How it works
+
+Until we have five matching letters that we can use to make an educated guess, we make a few exploratory guesses, aimed
+at cutting words from the dictionary based on letters that we know are not in the word while looking for . When we have
+five matching letters we try to make educated guesses. Luckily it doesn't look like there's a high number of anagrams
+for any given set of letters, so this should be safe enough to give the right answer by turn 4.
---
+++ src/main/scala/me/baghino/Wordle.scala
@@ -0,0 +1,91 @@
+package me.baghino
+
+import scala.collection.mutable
+import scala.io.Source
+import scala.util.Using
+
+final class Wordle(
+    dictionary: mutable.Set[String],
+    fuzzyMatch: mutable.Set[Char],
+    exactMatch: mutable.Map[Int, Char],
+) {
+
+  private var currentGuess: Option[String] = None
+
+  nextGuess()
+
+  def feedback(
+      doesNotMatch: String = "",
+      fuzzyMatch: String = "",
+      at0: Char = ' ',
+      at1: Char = ' ',
+      at2: Char = ' ',
+      at3: Char = ' ',
+      at4: Char = ' ',
+  ): Unit = {
+    if (at0.isLetter) exactMatch(0) = at0.toLower
+    if (at1.isLetter) exactMatch(1) = at1.toLower
+    if (at2.isLetter) exactMatch(2) = at2.toLower
+    if (at3.isLetter) exactMatch(3) = at3.toLower
+    if (at4.isLetter) exactMatch(4) = at4.toLower
+    val doesNotMatchSet = doesNotMatch.toSet
+    dictionary.filterInPlace(!_.exists(doesNotMatchSet))
+    currentGuess.foreach(dictionary -= _)
+    this.fuzzyMatch ++= fuzzyMatch.toSet
+    this.fuzzyMatch --= exactMatch.values.toSet
+
+    nextGuess()
+  }
+
+  private def nextGuess(): Unit = {
+    currentGuess = if (fuzzyMatch.size + exactMatch.size >= 5) {
+      educatedGuess().headOption
+    } else {
+      exploratoryGuess()
+    }
+    println(currentGuess.fold("I surrender.")(guess => s"My next guess is '$guess'."))
+  }
+
+  private def educatedGuess(): collection.Set[String] = {
+    dictionary.filter(word =>
+      exactMatch.forall { case (index, letter) =>
+        word(index) == letter
+      } && fuzzyMatch.forall(word.contains(_))
+    )
+  }
+
+  private def exploratoryGuess(): Option[String] = {
+    val weights = weigh(dictionary)
+    val exclusion = exactMatch.values.toSet ++ fuzzyMatch
+    dictionary
+      .filterNot(_.exists(exclusion))
+      .maxByOption(_.distinct.map(weights).sum)
+  }
+
+  private def weigh(dictionary: collection.Set[String]): Map[Char, Int] =
+    dictionary.view.reduce(_ + _).groupMapReduce(identity)(_ => 1)(_ + _)
+
+}
+
+object Wordle {
+
+  def loadAndCleanDictionary(path: String): Wordle =
+    new Wordle(
+      doLoadAndCleanDictionary(path),
+      mutable.Set.empty,
+      mutable.Map.empty,
+    )
+
+  private def doLoadAndCleanDictionary(path: String): mutable.Set[String] =
+    Using(Source.fromFile(path))(
+      _.getLines
+        .collect {
+          case word if valid(word) => word.toLowerCase
+        }
+        .to(mutable.Set)
+    ).get
+
+  private def valid(word: String): Boolean =
+    word.length == 5 && word.forall(_.isLetter)
+
+}
